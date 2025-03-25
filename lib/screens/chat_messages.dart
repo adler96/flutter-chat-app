af788bc7181f205235c42547bc74f79583b4f669
Moustapha LY <moustaphaly1996@gmail.com>
2025-03-25 23:26:41 +0000

    feature messaging is done

--- lib/screens/chat_messages.dart
+++ lib/screens/chat_messages.dart
@@ -1,11 +1,15 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_chat_app/widgets/message_bubble.dart';
 
 class ChatMessages extends StatelessWidget {
   const ChatMessages({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final authenticatedUser = FirebaseAuth.instance.currentUser;
+
     return StreamBuilder(
       stream: FirebaseFirestore.instance
           .collection('messages')
@@ -34,9 +38,34 @@ class ChatMessages extends StatelessWidget {
         final loadedMessages = chatSnapshots.data!.docs;
 
         return ListView.builder(
+          padding: EdgeInsets.only(left: 14, right: 14, bottom: 40),
+          reverse: true,
           itemCount: loadedMessages.length,
           itemBuilder: (ctx, index) {
-            return Text(loadedMessages[index].data()['text']);
+            final chatMessage = loadedMessages[index].data();
+            final nextChatMessage = index + 1 < loadedMessages.length
+                ? loadedMessages[index + 1].data()
+                : null;
+
+            final currentMessageUserId = chatMessage['userId'];
+            final nextMessageUserId =
+                nextChatMessage != null ? nextChatMessage['userId'] : null;
+
+            final nextUserIsSame = currentMessageUserId == nextMessageUserId;
+
+            if (nextUserIsSame) {
+              return MessageBubble.next(
+                message: chatMessage['text'],
+                isMe: authenticatedUser!.uid == currentMessageUserId,
+              );
+            } else {
+              return MessageBubble.first(
+                userImage: chatMessage['userImage'],
+                username: chatMessage['username'],
+                message: chatMessage['text'],
+                isMe: authenticatedUser!.uid == currentMessageUserId,
+              );
+            }
           },
         );
       },
