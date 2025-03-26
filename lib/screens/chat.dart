988d1fb00c4c00b49aaada65d21b538ac78b9078
Moustapha LY <moustaphaly1996@gmail.com>
2025-03-26 22:43:26 +0000

    app code complete

--- lib/screens/chat.dart
+++ lib/screens/chat.dart
@@ -1,10 +1,34 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_chat_app/screens/chat_messages.dart';
 import 'package:flutter_chat_app/screens/new_message.dart';
 
-class ChatScreen extends StatelessWidget {
+class ChatScreen extends StatefulWidget {
   const ChatScreen({super.key});
+
+  @override
+  State<ChatScreen> createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+
+  void setupPushNotifications() async {
+    final fcm = FirebaseMessaging.instance;
+    await fcm.requestPermission();
+    // we could detail permission request further after storing what the above yields
+
+    // each individual user needs one
+    final token = await fcm.getToken();
+    print(token); // this token could be send to a backend to do extra things with it
+    // like knowing who's online or whatever else
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    setupPushNotifications();
+  }
 
   @override
   Widget build(BuildContext context) {
